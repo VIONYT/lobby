0be4539f0571daa76ba694b69b5a7be8ee7fc294
VIONYT <yt.vion@gmail.com>
2024-05-26 16:46:38 +0200

    Added LuckyPerms as Permission System

--- src/main/java/moe/vion/lobby/Lobby.java
+++ src/main/java/moe/vion/lobby/Lobby.java
@@ -1,12 +1,18 @@
 package moe.vion.lobby;
 
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 
 public final class Lobby extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        @Nullable RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        Bukkit.getConsoleSender().sendMessage("Lobby-Plugin startet!");
 
     }
 
